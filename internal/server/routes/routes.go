a1ec8b9163bf8bf358666faffb63c70d242c7cf9
Abdul Aziz Alghifari <abdul.a.alghifari@gmail.com>
2023-02-14 12:31:56 +0700

    add routes

--- internal/server/routes/routes.go
+++ internal/server/routes/routes.go
@@ -5,6 +5,8 @@ import (
 	ctrls "ordent/internal/controller"
 	enUser "ordent/internal/entity/user"
 	"ordent/internal/server/routes/user"
+  "ordent/internal/server/routes/product"
+  "ordent/internal/server/routes/transaction"
 
   "github.com/golang-jwt/jwt/v4"
 	"github.com/labstack/echo-jwt/v4"
@@ -20,4 +22,6 @@ func Register(e *echo.Echo, controller *ctrls.Controllers, cfg config.JWT) {
     },
   }) 
   user.Register(e, controller, jwtMiddleware)
+  product.Register(e, controller, jwtMiddleware)
+  transaction.Register(e, controller, jwtMiddleware)
 }
